d4afb6b67a542930ed7e3317173c09a40a411a1d
paul uwaifo <uwaifopaul1@gmail.com>
2024-07-26 18:04:49 +0100

    fix deploment issue

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     env: {
-      REACT_APP_API_KEY: process.env.REACT_APP_API_KEY,
-      REACT_APP_ORGANIZATION_ID: process.env.REACT_APP_ORGANIZATION_ID,
-      REACT_APP_APP_ID: process.env.REACT_APP_APP_ID,
+      NEXT_PUBLIC_FIREBASE_API_KEY: "AIzaSyB-7U9WHh0db01c14U4a46pdr7InmyZEt0",
+      NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN_KEY: "link-sharing-app-662be.firebaseapp.com",
+      NEXT_PUBLIC_FIREBASE_PROJECT_ID: "link-sharing-app-662be",
+      NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET: "link-sharing-app-662be.appspot.com",
+      NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID: "421478092602",
+      NEXT_PUBLIC_FIREBASE_APP_ID: "1:421478092602:web:2e83580a03aee739918a03",
+      NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID: "G-7B5RJ7FLTV"
     },
   };
   
   export default nextConfig;
+  
   
